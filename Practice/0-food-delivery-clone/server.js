78f35416709e1e6a8aefc7dff90b6cc8b3887c19
achyutp10 <achyutp10@gmail.com>
2023-07-31 08:02:07 +0545

    adding reducers

--- Practice/0-food-delivery-clone/server.js
+++ Practice/0-food-delivery-clone/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 
+const Pizza = require("./models/pizzamodels");
+
+// const pizzas = require("./client/src/pizzasdata");
+
 const app = express();
+const db = require("./db");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send(`server Working on port ${port}`);
+});
+
+app.get("/getpizzas", async (req, res) => {
+  try {
+    const docs = await Pizza.find({});
+    res.send(docs);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occurred while fetching pizzas.");
+  }
 });
 
 const port = process.env.PORT || 8000;
